084f9ad14cb3d97b22f3ff80549a77ea316d5052
VasilyeuUV <vasilyeu.uv@mail.ru>
2018-07-03 02:50:32 +0300

    Binding UserControl and MainWindow. Attempt №1. No binding.

---
+++ Test/VM_ViewModels/VM_PanelFontParams.cs
@@ -0,0 +1,83 @@
+﻿using panelFontParams.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.VM_ViewModels
+{
+    public class VM_PanelFontParams : M_PropertyChanged
+    {
+
+        //#############################################################################################################################
+        #region FIELDS
+
+        private M_FontParams _testFontParam;
+
+        #endregion // FIELDS
+
+
+
+
+
+        //#############################################################################################################################
+        #region РЕАЛИЗАЦИЯ_SINGLETON
+
+        private static object syncRoot = new Object();
+        private static volatile VM_PanelFontParams _instance;
+        public static VM_PanelFontParams Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new VM_PanelFontParams();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+
+        #endregion // РЕАЛИЗАЦИЯ_SINGLETON
+
+
+
+
+
+
+        //#############################################################################################################################
+        #region PROPERTIES
+
+        public M_FontParams TestFontParam
+        {
+            get
+            {
+                if (_testFontParam == null)
+                {
+                    _testFontParam = panelFontParams.VM_ViewModels.VM_FontParam.Instance.UcFontParam;                    
+                }
+                return _testFontParam;
+            }
+            set
+            {
+                _testFontParam = value;
+                OnPropertyChanged("TestFontParam");
+            }
+        }
+
+
+        #endregion // PROPERTIES
+
+
+    }
+
+
+}
+
